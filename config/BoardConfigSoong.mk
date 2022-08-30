1bc94637071f656456e89afe81494da330eaff34
Michael Bestas <mkbestas@lineageos.org>
2022-08-30 03:13:55 +0300

    config: Remove TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE
    This hasn't been ported after lineage-17.1
    
    Change-Id: I2aff9bb7bc17fa1444a904d47248d885268bf29b

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -34,7 +34,6 @@ SOONG_CONFIG_octaviGlobalVars += \
     camera_needs_client_info_lib \
     disable_postrender_cleanup \
     has_memfd_backport \
-    ignores_ftp_pptp_conntrack_failure \
     needs_camera_boottime \
     needs_netd_direct_connect_rule \
     target_alternative_futex_waiters \
@@ -66,7 +65,6 @@ endif
 # Soong bool variables
 SOONG_CONFIG_octaviGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_POSTRENDER_CLEANUP)
 SOONG_CONFIG_octaviGlobalVars_has_memfd_backport := $(TARGET_HAS_MEMFD_BACKPORT)
-SOONG_CONFIG_octaviGlobalVars_ignores_ftp_pptp_conntrack_failure := $(TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE)
 SOONG_CONFIG_octaviGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 SOONG_CONFIG_octaviGlobalVars_needs_netd_direct_connect_rule := $(TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE)
 SOONG_CONFIG_octaviGlobalVars_target_alternative_futex_waiters := $(TARGET_ALTERNATIVE_FUTEX_WAITERS)
