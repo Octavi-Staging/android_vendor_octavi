nitrogen-project/android_vendor_nitrogen

Import gms packages for Smartspace

products/common.mk
@@ -118,6 +118,15 @@ PRODUCT_PACKAGES += \
     ThemePicker \
     DefaultThemesStub
 
+# Smartspace
+PRODUCT_PACKAGES += \
+    DeviceIntelligenceNetworkPrebuilt \
+    DevicePersonalizationPrebuiltPixel2021 \
+    PixelConfigOverlayCommon
+
+PRODUCT_COPY_FILES += \
+    vendor/nitrogen/prebuilt/common/etc/permissions/privapp-permissions-google-as.xml:product/etc/permissions/privapp-permissions-google-as.xml
+
 # Default notification/alarm/ringtone sounds
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.config.notification_sound=Argon.ogg \

Octavi-Staging/android_vendor_octavi

config/common.mk
@@ -138,6 +138,15 @@ endif
 # Fonts
 include vendor/octavi/config/fonts.mk
 
+# Smartspace
+PRODUCT_PACKAGES += \
+    DeviceIntelligenceNetworkPrebuilt \
+    DevicePersonalizationPrebuiltPixel2021 \
+    PixelConfigOverlayCommon
+
+PRODUCT_COPY_FILES += \
+    vendor/octavi/prebuilt/common/etc/permissions/privapp-permissions-google-as.xml:product/etc/permissions/privapp-permissions-google-as.xml
+
 # Themes
 $(call inherit-product, vendor/octavi/config/themes.mk)
 -include $(WORKSPACE)/build_env/image-auto-bits.mk