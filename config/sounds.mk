maldini03/vendor_banana

vendor: sounds: add AOSP Material sounds
The default AOSP ringtone, alarm, and notification sounds are quite
lackluster in comparison to the proprietary Pixel sounds. Replace them
with better ones that are licensed permissively:
  - AOSP Material sounds from Android 9
    frameworks/base/data/sounds/AudioPackage14.mk
  - Material Product Sounds [1], licensed under CC-BY 4.0
    https://material.io/design/sound/sound-resources.html

Change-Id: Id69b3fb015ad8a5489f3186a75eb5815c34817eb

config/sounds.mk
@@ -17,6 +17,11 @@
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,vendor/banana/sounds/pixel/,$(TARGET_COPY_OUT_PRODUCT)/media/audio)
 
+# 2019 Material product sounds (CC-BY 4.0)
+# Source: https://material.io/design/sound/sound-resources.html
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/banana/sounds/material/,$(TARGET_COPY_OUT_PRODUCT)/media/audio)
+
 # Set default sounds
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.config.alarm_alert=Bright_morning.ogg \

Octavi-Staging/android_vendor_octavi

config/sounds.mk
@@ -17,6 +17,11 @@
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,vendor/octavi/sounds/pixel/,$(TARGET_COPY_OUT_PRODUCT)/media/audio)
 
+# 2019 Material product sounds (CC-BY 4.0)
+# Source: https://material.io/design/sound/sound-resources.html
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/octavi/sounds/material/,$(TARGET_COPY_OUT_PRODUCT)/media/audio)
+
 # Set default sounds
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.config.alarm_alert=Bright_morning.ogg \