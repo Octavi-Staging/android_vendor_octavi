maldini03/vendor_banana

vendor: sounds: Update to redfin
 * Relocate in pixel folder.
 * Edite metadata for sound files (Replace pixel SoundsPicker strings on names)
 * Hold only redfin sounds.

Change-Id: I24340f87f90c7393e58b513ccd310383c9d77755

config/sounds.mk
@@ -1,4 +1,5 @@
-# Copyright 2016-2017 The Pure Nexus Project
+# Copyright (C) 2016-2017 The Pure Nexus Project
+# Copyright (C) 2018-2021 ArrowOS
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,128 +13,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Pixel Sounds - redfin-user 11 RQ3A.210705.001 7380771 release-keys
 PRODUCT_COPY_FILES += \
-	vendor/banana/sounds/alarms/A_real_hoot.ogg:system/media/audio/alarms/A_real_hoot.ogg \
-	vendor/banana/sounds/alarms/Argon.ogg:system/media/audio/alarms/Argon.ogg \
-	vendor/banana/sounds/alarms/Awaken.ogg:system/media/audio/alarms/Awaken.ogg \
-	vendor/banana/sounds/alarms/Bounce.ogg:system/media/audio/alarms/Bounce.ogg \
-	vendor/banana/sounds/alarms/Bright_morning.ogg:system/media/audio/alarms/Bright_morning.ogg \
-	vendor/banana/sounds/alarms/Carbon.ogg:system/media/audio/alarms/Carbon.ogg \
-	vendor/banana/sounds/alarms/Cuckoo_clock.ogg:system/media/audio/alarms/Cuckoo_clock.ogg \
-	vendor/banana/sounds/alarms/Drip.ogg:system/media/audio/alarms/Drip.ogg \
-	vendor/banana/sounds/alarms/Early_twilight.ogg:system/media/audio/alarms/Early_twilight.ogg \
-	vendor/banana/sounds/alarms/Flow.ogg:system/media/audio/alarms/Flow.ogg \
-	vendor/banana/sounds/alarms/Full_of_wonder.ogg:system/media/audio/alarms/Full_of_wonder.ogg \
-	vendor/banana/sounds/alarms/Gallop.ogg:system/media/audio/alarms/Gallop.ogg \
-	vendor/banana/sounds/alarms/Gentle_breeze.ogg:system/media/audio/alarms/Gentle_breeze.ogg \
-	vendor/banana/sounds/alarms/Helium.ogg:system/media/audio/alarms/Helium.ogg \
-	vendor/banana/sounds/alarms/Icicles.ogg:system/media/audio/alarms/Icicles.ogg \
-	vendor/banana/sounds/alarms/Jump_start.ogg:system/media/audio/alarms/Jump_start.ogg \
-	vendor/banana/sounds/alarms/Krypton.ogg:system/media/audio/alarms/Krypton.ogg \
-	vendor/banana/sounds/alarms/Loose_change.ogg:system/media/audio/alarms/Loose_change.ogg \
-	vendor/banana/sounds/alarms/Neon.ogg:system/media/audio/alarms/Neon.ogg \
-	vendor/banana/sounds/alarms/Nudge.ogg:system/media/audio/alarms/Nudge.ogg \
-	vendor/banana/sounds/alarms/Orbit.ogg:system/media/audio/alarms/Orbit.ogg \
-	vendor/banana/sounds/alarms/Rise.ogg:system/media/audio/alarms/Rise.ogg \
-	vendor/banana/sounds/alarms/Rolling_fog.ogg:system/media/audio/alarms/Rolling_fog.ogg \
-	vendor/banana/sounds/alarms/Spokes.ogg:system/media/audio/alarms/Spokes.ogg \
-	vendor/banana/sounds/alarms/Sunshower.ogg:system/media/audio/alarms/Sunshower.ogg \
-	vendor/banana/sounds/alarms/Sway.ogg:system/media/audio/alarms/Sway.ogg \
-	vendor/banana/sounds/alarms/Timer.ogg:system/media/audio/alarms/Timer.ogg \
-	vendor/banana/sounds/alarms/Wag.ogg:system/media/audio/alarms/Wag.ogg \
-	vendor/banana/sounds/ui/audio_end.ogg:system/media/audio/ui/audio_end.ogg \
-	vendor/banana/sounds/ui/audio_initiate.ogg:system/media/audio/ui/audio_initiate.ogg \
-	vendor/banana/sounds/ui/camera_click.ogg:system/media/audio/ui/camera_click.ogg \
-	vendor/banana/sounds/ui/camera_focus.ogg:system/media/audio/ui/camera_focus.ogg \
-	vendor/banana/sounds/ui/ChargingStarted.ogg:system/media/audio/ui/ChargingStarted.ogg \
-	vendor/banana/sounds/ui/Dock.ogg:system/media/audio/ui/Dock.ogg \
-	vendor/banana/sounds/ui/Effect_Tick.ogg:system/media/audio/ui/Effect_Tick.ogg \
-	vendor/banana/sounds/ui/KeypressDelete.ogg:system/media/audio/ui/KeypressDelete.ogg \
-	vendor/banana/sounds/ui/KeypressInvalid.ogg:system/media/audio/ui/KeypressInvalid.ogg \
-	vendor/banana/sounds/ui/KeypressReturn.ogg:system/media/audio/ui/KeypressReturn.ogg \
-	vendor/banana/sounds/ui/KeypressSpacebar.ogg:system/media/audio/ui/KeypressSpacebar.ogg \
-	vendor/banana/sounds/ui/KeypressStandard.ogg:system/media/audio/ui/KeypressStandard.ogg \
-	vendor/banana/sounds/ui/Lock.ogg:system/media/audio/ui/Lock.ogg \
-	vendor/banana/sounds/ui/LowBattery.ogg:system/media/audio/ui/LowBattery.ogg \
-	vendor/banana/sounds/ui/NFCFailure.ogg:system/media/audio/ui/NFCFailure.ogg \
-	vendor/banana/sounds/ui/NFCInitiated.ogg:system/media/audio/ui/NFCInitiated.ogg \
-	vendor/banana/sounds/ui/NFCSuccess.ogg:system/media/audio/ui/NFCSuccess.ogg \
-	vendor/banana/sounds/ui/NFCTransferComplete.ogg:system/media/audio/ui/NFCTransferComplete.ogg \
-	vendor/banana/sounds/ui/NFCTransferInitiated.ogg:system/media/audio/ui/NFCTransferInitiated.ogg \
-	vendor/banana/sounds/ui/Trusted.ogg:system/media/audio/ui/Trusted.ogg \
-	vendor/banana/sounds/ui/Undock.ogg:system/media/audio/ui/Undock.ogg \
-	vendor/banana/sounds/ui/Unlock.ogg:system/media/audio/ui/Unlock.ogg \
-	vendor/banana/sounds/ui/VideoRecord.ogg:system/media/audio/ui/VideoRecord.ogg \
-	vendor/banana/sounds/ui/VideoStop.ogg:system/media/audio/ui/VideoStop.ogg \
-	vendor/banana/sounds/ui/WirelessChargingStarted.ogg:system/media/audio/ui/WirelessChargingStarted.ogg \
-	vendor/banana/sounds/notifications/Ariel.ogg:system/media/audio/notifications/Ariel.ogg \
-	vendor/banana/sounds/notifications/Beginning.ogg:system/media/audio/notifications/Beginning.ogg \
-	vendor/banana/sounds/notifications/Birdsong.ogg:system/media/audio/notifications/Birdsong.ogg \
-	vendor/banana/sounds/notifications/Carme.ogg:system/media/audio/notifications/Carme.ogg \
-	vendor/banana/sounds/notifications/Ceres.ogg:system/media/audio/notifications/Ceres.ogg \
-	vendor/banana/sounds/notifications/Chime.ogg:system/media/audio/notifications/Chime.ogg \
-	vendor/banana/sounds/notifications/Clink.ogg:system/media/audio/notifications/Clink.ogg \
-	vendor/banana/sounds/notifications/Coconuts.ogg:system/media/audio/notifications/Coconuts.ogg \
-	vendor/banana/sounds/notifications/Duet.ogg:system/media/audio/notifications/Duet.ogg \
-	vendor/banana/sounds/notifications/Elara.ogg:system/media/audio/notifications/Elara.ogg \
-	vendor/banana/sounds/notifications/End_note.ogg:system/media/audio/notifications/End_note.ogg \
-	vendor/banana/sounds/notifications/Europa.ogg:system/media/audio/notifications/Europa.ogg \
-	vendor/banana/sounds/notifications/Flick.ogg:system/media/audio/notifications/Flick.ogg \
-	vendor/banana/sounds/notifications/Gentle_gong.ogg:system/media/audio/notifications/Gentle_gong.ogg \
-	vendor/banana/sounds/notifications/Hey.ogg:system/media/audio/notifications/Hey.ogg \
-	vendor/banana/sounds/notifications/Iapetus.ogg:system/media/audio/notifications/Iapetus.ogg \
-	vendor/banana/sounds/notifications/Io.ogg:system/media/audio/notifications/Io.ogg \
-	vendor/banana/sounds/notifications/Mallet.ogg:system/media/audio/notifications/Mallet.ogg \
-	vendor/banana/sounds/notifications/Note.ogg:system/media/audio/notifications/Note.ogg \
-	vendor/banana/sounds/notifications/Orders_up.ogg:system/media/audio/notifications/Orders_up.ogg \
-	vendor/banana/sounds/notifications/Ping.ogg:system/media/audio/notifications/Ping.ogg \
-	vendor/banana/sounds/notifications/Pipes.ogg:system/media/audio/notifications/Pipes.ogg \
-	vendor/banana/sounds/notifications/Popcorn.ogg:system/media/audio/notifications/Popcorn.ogg \
-	vendor/banana/sounds/notifications/Rhea.ogg:system/media/audio/notifications/Rhea.ogg \
-	vendor/banana/sounds/notifications/Salacia.ogg:system/media/audio/notifications/Salacia.ogg \
-	vendor/banana/sounds/notifications/Shopkeeper.ogg:system/media/audio/notifications/Shopkeeper.ogg \
-	vendor/banana/sounds/notifications/Sticks_and_stones.ogg:system/media/audio/notifications/Sticks_and_stones.ogg \
-	vendor/banana/sounds/notifications/Strum.ogg:system/media/audio/notifications/Strum.ogg \
-	vendor/banana/sounds/notifications/Tethys.ogg:system/media/audio/notifications/Tethys.ogg \
-	vendor/banana/sounds/notifications/Titan.ogg:system/media/audio/notifications/Titan.ogg \
-	vendor/banana/sounds/notifications/Trill.ogg:system/media/audio/notifications/Trill.ogg \
-	vendor/banana/sounds/notifications/Tuneup.ogg:system/media/audio/notifications/Tuneup.ogg \
-	vendor/banana/sounds/notifications/Tweeter.ogg:system/media/audio/notifications/Tweeter.ogg \
-	vendor/banana/sounds/notifications/Twinkle.ogg:system/media/audio/notifications/Twinkle.ogg \
-	vendor/banana/sounds/ringtones/Atria.ogg:system/media/audio/ringtones/Atria.ogg \
-	vendor/banana/sounds/ringtones/Beats.ogg:system/media/audio/ringtones/Beats.ogg \
-	vendor/banana/sounds/ringtones/Callisto.ogg:system/media/audio/ringtones/Callisto.ogg \
-	vendor/banana/sounds/ringtones/Copycat.ogg:system/media/audio/ringtones/Copycat.ogg \
-	vendor/banana/sounds/ringtones/Crackle.ogg:system/media/audio/ringtones/Crackle.ogg \
-	vendor/banana/sounds/ringtones/Dance_party.ogg:system/media/audio/ringtones/Dance_party.ogg \
-	vendor/banana/sounds/ringtones/Dione.ogg:system/media/audio/ringtones/Dione.ogg \
-	vendor/banana/sounds/ringtones/Early_bird.ogg:system/media/audio/ringtones/Early_bird.ogg \
-	vendor/banana/sounds/ringtones/Flutterby.ogg:system/media/audio/ringtones/Flutterby.ogg \
-	vendor/banana/sounds/ringtones/Ganymede.ogg:system/media/audio/ringtones/Ganymede.ogg \
-	vendor/banana/sounds/ringtones/Hey_hey.ogg:system/media/audio/ringtones/Hey_hey.ogg \
-	vendor/banana/sounds/ringtones/Hotline.ogg:system/media/audio/ringtones/Hotline.ogg \
-	vendor/banana/sounds/ringtones/Leaps_and_bounds.ogg:system/media/audio/ringtones/Leaps_and_bounds.ogg \
-	vendor/banana/sounds/ringtones/Lollipop.ogg:system/media/audio/ringtones/Lollipop.ogg \
-	vendor/banana/sounds/ringtones/Lost_and_found.ogg:system/media/audio/ringtones/Lost_and_found.ogg \
-	vendor/banana/sounds/ringtones/Luna.ogg:system/media/audio/ringtones/Luna.ogg \
-	vendor/banana/sounds/ringtones/Mash_up.ogg:system/media/audio/ringtones/Mash_up.ogg \
-	vendor/banana/sounds/ringtones/Monkey_around.ogg:system/media/audio/ringtones/Monkey_around.ogg \
-	vendor/banana/sounds/ringtones/Oberon.ogg:system/media/audio/ringtones/Oberon.ogg \
-	vendor/banana/sounds/ringtones/Phobos.ogg:system/media/audio/ringtones/Phobos.ogg \
-	vendor/banana/sounds/ringtones/Pyxis.ogg:system/media/audio/ringtones/Pyxis.ogg \
-	vendor/banana/sounds/ringtones/Rrrring.ogg:system/media/audio/ringtones/Rrrring.ogg \
-	vendor/banana/sounds/ringtones/Sedna.ogg:system/media/audio/ringtones/Sedna.ogg \
-	vendor/banana/sounds/ringtones/Schools_out.ogg:system/media/audio/ringtones/Schools_out.ogg \
-	vendor/banana/sounds/ringtones/Shooting_star.ogg:system/media/audio/ringtones/Shooting_star.ogg \
-	vendor/banana/sounds/ringtones/Spaceship.ogg:system/media/audio/ringtones/Spaceship.ogg \
-	vendor/banana/sounds/ringtones/Summer_night.ogg:system/media/audio/ringtones/Summer_night.ogg \
-	vendor/banana/sounds/ringtones/The_big_adventure.ogg:system/media/audio/ringtones/The_big_adventure.ogg \
-	vendor/banana/sounds/ringtones/Titania.ogg:system/media/audio/ringtones/Titania.ogg \
-	vendor/banana/sounds/ringtones/Triton.ogg:system/media/audio/ringtones/Triton.ogg \
-	vendor/banana/sounds/ringtones/Umbriel.ogg:system/media/audio/ringtones/Umbriel.ogg \
-	vendor/banana/sounds/ringtones/Zen.ogg:system/media/audio/ringtones/Zen.ogg \
-	vendor/banana/sounds/ringtones/Zen_too.ogg:system/media/audio/ringtones/Zen_too.ogg
+    $(call find-copy-subdir-files,*,vendor/banana/sounds/pixel/,$(TARGET_COPY_OUT_PRODUCT)/media/audio)
 
-PRODUCT_PROPERTY_OVERRIDES += \
-   ro.config.ringtone=Zen_too.ogg \
-   ro.config.notification_sound=Chime.ogg
+# Set default sounds
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.config.alarm_alert=Bright_morning.ogg \
+    ro.config.notification_sound=End_note.ogg \
+    ro.config.ringtone=The_big_adventure.ogg

Octavi-Staging/android_vendor_octavi

config/sounds.mk
@@ -1,4 +1,5 @@
-# Copyright 2016-2017 The Pure Nexus Project
+# Copyright (C) 2016-2017 The Pure Nexus Project
+# Copyright (C) 2018-2021 ArrowOS
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,128 +13,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Pixel Sounds - redfin-user 11 RQ3A.210705.001 7380771 release-keys
 PRODUCT_COPY_FILES += \
-	vendor/octavi/sounds/alarms/A_real_hoot.ogg:system/media/audio/alarms/A_real_hoot.ogg \
-	vendor/octavi/sounds/alarms/Argon.ogg:system/media/audio/alarms/Argon.ogg \
-	vendor/octavi/sounds/alarms/Awaken.ogg:system/media/audio/alarms/Awaken.ogg \
-	vendor/octavi/sounds/alarms/Bounce.ogg:system/media/audio/alarms/Bounce.ogg \
-	vendor/octavi/sounds/alarms/Bright_morning.ogg:system/media/audio/alarms/Bright_morning.ogg \
-	vendor/octavi/sounds/alarms/Carbon.ogg:system/media/audio/alarms/Carbon.ogg \
-	vendor/octavi/sounds/alarms/Cuckoo_clock.ogg:system/media/audio/alarms/Cuckoo_clock.ogg \
-	vendor/octavi/sounds/alarms/Drip.ogg:system/media/audio/alarms/Drip.ogg \
-	vendor/octavi/sounds/alarms/Early_twilight.ogg:system/media/audio/alarms/Early_twilight.ogg \
-	vendor/octavi/sounds/alarms/Flow.ogg:system/media/audio/alarms/Flow.ogg \
-	vendor/octavi/sounds/alarms/Full_of_wonder.ogg:system/media/audio/alarms/Full_of_wonder.ogg \
-	vendor/octavi/sounds/alarms/Gallop.ogg:system/media/audio/alarms/Gallop.ogg \
-	vendor/octavi/sounds/alarms/Gentle_breeze.ogg:system/media/audio/alarms/Gentle_breeze.ogg \
-	vendor/octavi/sounds/alarms/Helium.ogg:system/media/audio/alarms/Helium.ogg \
-	vendor/octavi/sounds/alarms/Icicles.ogg:system/media/audio/alarms/Icicles.ogg \
-	vendor/octavi/sounds/alarms/Jump_start.ogg:system/media/audio/alarms/Jump_start.ogg \
-	vendor/octavi/sounds/alarms/Krypton.ogg:system/media/audio/alarms/Krypton.ogg \
-	vendor/octavi/sounds/alarms/Loose_change.ogg:system/media/audio/alarms/Loose_change.ogg \
-	vendor/octavi/sounds/alarms/Neon.ogg:system/media/audio/alarms/Neon.ogg \
-	vendor/octavi/sounds/alarms/Nudge.ogg:system/media/audio/alarms/Nudge.ogg \
-	vendor/octavi/sounds/alarms/Orbit.ogg:system/media/audio/alarms/Orbit.ogg \
-	vendor/octavi/sounds/alarms/Rise.ogg:system/media/audio/alarms/Rise.ogg \
-	vendor/octavi/sounds/alarms/Rolling_fog.ogg:system/media/audio/alarms/Rolling_fog.ogg \
-	vendor/octavi/sounds/alarms/Spokes.ogg:system/media/audio/alarms/Spokes.ogg \
-	vendor/octavi/sounds/alarms/Sunshower.ogg:system/media/audio/alarms/Sunshower.ogg \
-	vendor/octavi/sounds/alarms/Sway.ogg:system/media/audio/alarms/Sway.ogg \
-	vendor/octavi/sounds/alarms/Timer.ogg:system/media/audio/alarms/Timer.ogg \
-	vendor/octavi/sounds/alarms/Wag.ogg:system/media/audio/alarms/Wag.ogg \
-	vendor/octavi/sounds/ui/audio_end.ogg:system/media/audio/ui/audio_end.ogg \
-	vendor/octavi/sounds/ui/audio_initiate.ogg:system/media/audio/ui/audio_initiate.ogg \
-	vendor/octavi/sounds/ui/camera_click.ogg:system/media/audio/ui/camera_click.ogg \
-	vendor/octavi/sounds/ui/camera_focus.ogg:system/media/audio/ui/camera_focus.ogg \
-	vendor/octavi/sounds/ui/ChargingStarted.ogg:system/media/audio/ui/ChargingStarted.ogg \
-	vendor/octavi/sounds/ui/Dock.ogg:system/media/audio/ui/Dock.ogg \
-	vendor/octavi/sounds/ui/Effect_Tick.ogg:system/media/audio/ui/Effect_Tick.ogg \
-	vendor/octavi/sounds/ui/KeypressDelete.ogg:system/media/audio/ui/KeypressDelete.ogg \
-	vendor/octavi/sounds/ui/KeypressInvalid.ogg:system/media/audio/ui/KeypressInvalid.ogg \
-	vendor/octavi/sounds/ui/KeypressReturn.ogg:system/media/audio/ui/KeypressReturn.ogg \
-	vendor/octavi/sounds/ui/KeypressSpacebar.ogg:system/media/audio/ui/KeypressSpacebar.ogg \
-	vendor/octavi/sounds/ui/KeypressStandard.ogg:system/media/audio/ui/KeypressStandard.ogg \
-	vendor/octavi/sounds/ui/Lock.ogg:system/media/audio/ui/Lock.ogg \
-	vendor/octavi/sounds/ui/LowBattery.ogg:system/media/audio/ui/LowBattery.ogg \
-	vendor/octavi/sounds/ui/NFCFailure.ogg:system/media/audio/ui/NFCFailure.ogg \
-	vendor/octavi/sounds/ui/NFCInitiated.ogg:system/media/audio/ui/NFCInitiated.ogg \
-	vendor/octavi/sounds/ui/NFCSuccess.ogg:system/media/audio/ui/NFCSuccess.ogg \
-	vendor/octavi/sounds/ui/NFCTransferComplete.ogg:system/media/audio/ui/NFCTransferComplete.ogg \
-	vendor/octavi/sounds/ui/NFCTransferInitiated.ogg:system/media/audio/ui/NFCTransferInitiated.ogg \
-	vendor/octavi/sounds/ui/Trusted.ogg:system/media/audio/ui/Trusted.ogg \
-	vendor/octavi/sounds/ui/Undock.ogg:system/media/audio/ui/Undock.ogg \
-	vendor/octavi/sounds/ui/Unlock.ogg:system/media/audio/ui/Unlock.ogg \
-	vendor/octavi/sounds/ui/VideoRecord.ogg:system/media/audio/ui/VideoRecord.ogg \
-	vendor/octavi/sounds/ui/VideoStop.ogg:system/media/audio/ui/VideoStop.ogg \
-	vendor/octavi/sounds/ui/WirelessChargingStarted.ogg:system/media/audio/ui/WirelessChargingStarted.ogg \
-	vendor/octavi/sounds/notifications/Ariel.ogg:system/media/audio/notifications/Ariel.ogg \
-	vendor/octavi/sounds/notifications/Beginning.ogg:system/media/audio/notifications/Beginning.ogg \
-	vendor/octavi/sounds/notifications/Birdsong.ogg:system/media/audio/notifications/Birdsong.ogg \
-	vendor/octavi/sounds/notifications/Carme.ogg:system/media/audio/notifications/Carme.ogg \
-	vendor/octavi/sounds/notifications/Ceres.ogg:system/media/audio/notifications/Ceres.ogg \
-	vendor/octavi/sounds/notifications/Chime.ogg:system/media/audio/notifications/Chime.ogg \
-	vendor/octavi/sounds/notifications/Clink.ogg:system/media/audio/notifications/Clink.ogg \
-	vendor/octavi/sounds/notifications/Coconuts.ogg:system/media/audio/notifications/Coconuts.ogg \
-	vendor/octavi/sounds/notifications/Duet.ogg:system/media/audio/notifications/Duet.ogg \
-	vendor/octavi/sounds/notifications/Elara.ogg:system/media/audio/notifications/Elara.ogg \
-	vendor/octavi/sounds/notifications/End_note.ogg:system/media/audio/notifications/End_note.ogg \
-	vendor/octavi/sounds/notifications/Europa.ogg:system/media/audio/notifications/Europa.ogg \
-	vendor/octavi/sounds/notifications/Flick.ogg:system/media/audio/notifications/Flick.ogg \
-	vendor/octavi/sounds/notifications/Gentle_gong.ogg:system/media/audio/notifications/Gentle_gong.ogg \
-	vendor/octavi/sounds/notifications/Hey.ogg:system/media/audio/notifications/Hey.ogg \
-	vendor/octavi/sounds/notifications/Iapetus.ogg:system/media/audio/notifications/Iapetus.ogg \
-	vendor/octavi/sounds/notifications/Io.ogg:system/media/audio/notifications/Io.ogg \
-	vendor/octavi/sounds/notifications/Mallet.ogg:system/media/audio/notifications/Mallet.ogg \
-	vendor/octavi/sounds/notifications/Note.ogg:system/media/audio/notifications/Note.ogg \
-	vendor/octavi/sounds/notifications/Orders_up.ogg:system/media/audio/notifications/Orders_up.ogg \
-	vendor/octavi/sounds/notifications/Ping.ogg:system/media/audio/notifications/Ping.ogg \
-	vendor/octavi/sounds/notifications/Pipes.ogg:system/media/audio/notifications/Pipes.ogg \
-	vendor/octavi/sounds/notifications/Popcorn.ogg:system/media/audio/notifications/Popcorn.ogg \
-	vendor/octavi/sounds/notifications/Rhea.ogg:system/media/audio/notifications/Rhea.ogg \
-	vendor/octavi/sounds/notifications/Salacia.ogg:system/media/audio/notifications/Salacia.ogg \
-	vendor/octavi/sounds/notifications/Shopkeeper.ogg:system/media/audio/notifications/Shopkeeper.ogg \
-	vendor/octavi/sounds/notifications/Sticks_and_stones.ogg:system/media/audio/notifications/Sticks_and_stones.ogg \
-	vendor/octavi/sounds/notifications/Strum.ogg:system/media/audio/notifications/Strum.ogg \
-	vendor/octavi/sounds/notifications/Tethys.ogg:system/media/audio/notifications/Tethys.ogg \
-	vendor/octavi/sounds/notifications/Titan.ogg:system/media/audio/notifications/Titan.ogg \
-	vendor/octavi/sounds/notifications/Trill.ogg:system/media/audio/notifications/Trill.ogg \
-	vendor/octavi/sounds/notifications/Tuneup.ogg:system/media/audio/notifications/Tuneup.ogg \
-	vendor/octavi/sounds/notifications/Tweeter.ogg:system/media/audio/notifications/Tweeter.ogg \
-	vendor/octavi/sounds/notifications/Twinkle.ogg:system/media/audio/notifications/Twinkle.ogg \
-	vendor/octavi/sounds/ringtones/Atria.ogg:system/media/audio/ringtones/Atria.ogg \
-	vendor/octavi/sounds/ringtones/Beats.ogg:system/media/audio/ringtones/Beats.ogg \
-	vendor/octavi/sounds/ringtones/Callisto.ogg:system/media/audio/ringtones/Callisto.ogg \
-	vendor/octavi/sounds/ringtones/Copycat.ogg:system/media/audio/ringtones/Copycat.ogg \
-	vendor/octavi/sounds/ringtones/Crackle.ogg:system/media/audio/ringtones/Crackle.ogg \
-	vendor/octavi/sounds/ringtones/Dance_party.ogg:system/media/audio/ringtones/Dance_party.ogg \
-	vendor/octavi/sounds/ringtones/Dione.ogg:system/media/audio/ringtones/Dione.ogg \
-	vendor/octavi/sounds/ringtones/Early_bird.ogg:system/media/audio/ringtones/Early_bird.ogg \
-	vendor/octavi/sounds/ringtones/Flutterby.ogg:system/media/audio/ringtones/Flutterby.ogg \
-	vendor/octavi/sounds/ringtones/Ganymede.ogg:system/media/audio/ringtones/Ganymede.ogg \
-	vendor/octavi/sounds/ringtones/Hey_hey.ogg:system/media/audio/ringtones/Hey_hey.ogg \
-	vendor/octavi/sounds/ringtones/Hotline.ogg:system/media/audio/ringtones/Hotline.ogg \
-	vendor/octavi/sounds/ringtones/Leaps_and_bounds.ogg:system/media/audio/ringtones/Leaps_and_bounds.ogg \
-	vendor/octavi/sounds/ringtones/Lollipop.ogg:system/media/audio/ringtones/Lollipop.ogg \
-	vendor/octavi/sounds/ringtones/Lost_and_found.ogg:system/media/audio/ringtones/Lost_and_found.ogg \
-	vendor/octavi/sounds/ringtones/Luna.ogg:system/media/audio/ringtones/Luna.ogg \
-	vendor/octavi/sounds/ringtones/Mash_up.ogg:system/media/audio/ringtones/Mash_up.ogg \
-	vendor/octavi/sounds/ringtones/Monkey_around.ogg:system/media/audio/ringtones/Monkey_around.ogg \
-	vendor/octavi/sounds/ringtones/Oberon.ogg:system/media/audio/ringtones/Oberon.ogg \
-	vendor/octavi/sounds/ringtones/Phobos.ogg:system/media/audio/ringtones/Phobos.ogg \
-	vendor/octavi/sounds/ringtones/Pyxis.ogg:system/media/audio/ringtones/Pyxis.ogg \
-	vendor/octavi/sounds/ringtones/Rrrring.ogg:system/media/audio/ringtones/Rrrring.ogg \
-	vendor/octavi/sounds/ringtones/Sedna.ogg:system/media/audio/ringtones/Sedna.ogg \
-	vendor/octavi/sounds/ringtones/Schools_out.ogg:system/media/audio/ringtones/Schools_out.ogg \
-	vendor/octavi/sounds/ringtones/Shooting_star.ogg:system/media/audio/ringtones/Shooting_star.ogg \
-	vendor/octavi/sounds/ringtones/Spaceship.ogg:system/media/audio/ringtones/Spaceship.ogg \
-	vendor/octavi/sounds/ringtones/Summer_night.ogg:system/media/audio/ringtones/Summer_night.ogg \
-	vendor/octavi/sounds/ringtones/The_big_adventure.ogg:system/media/audio/ringtones/The_big_adventure.ogg \
-	vendor/octavi/sounds/ringtones/Titania.ogg:system/media/audio/ringtones/Titania.ogg \
-	vendor/octavi/sounds/ringtones/Triton.ogg:system/media/audio/ringtones/Triton.ogg \
-	vendor/octavi/sounds/ringtones/Umbriel.ogg:system/media/audio/ringtones/Umbriel.ogg \
-	vendor/octavi/sounds/ringtones/Zen.ogg:system/media/audio/ringtones/Zen.ogg \
-	vendor/octavi/sounds/ringtones/Zen_too.ogg:system/media/audio/ringtones/Zen_too.ogg
+    $(call find-copy-subdir-files,*,vendor/octavi/sounds/pixel/,$(TARGET_COPY_OUT_PRODUCT)/media/audio)
 
-PRODUCT_PROPERTY_OVERRIDES += \
-   ro.config.ringtone=Zen_too.ogg \
-   ro.config.notification_sound=Chime.ogg
+# Set default sounds
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.config.alarm_alert=Bright_morning.ogg \
+    ro.config.ringtone=The_big_adventure.ogg