yaap/vendor_yaap

config: sepolicy: import flipendo/turbo_adapter sepolicy
Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

config/common.mk
@@ -136,3 +136,6 @@ PRODUCT_HOST_PACKAGES += \
 
 # Themes
 $(call inherit-product, vendor/themes/common.mk)
+
+# Sepolicy
+$(call inherit-product, vendor/yaap/config/sepolicy.mk)

Octavi-Staging/android_vendor_octavi

config/common.mk
@@ -142,3 +142,6 @@ EXCLUDE_SYSTEMUI_TESTS := true
 # Themes
 $(call inherit-product, vendor/octavi/config/themes.mk)
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
+
+# Sepolicy
+$(call inherit-product, vendor/octavi/config/sepolicy.mk)