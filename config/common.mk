84ab5d754f85d4d6f6e1a0b5730daa82e232c001
rmp22 <195054967+rmp22@users.noreply.github.com>
2024-06-17 19:55:36 +0900

    config: add blur support
    Change-Id: I5fabdbbdfe812346f17dbba5d4bf9abe7344e0ca

--- config/common.mk
+++ config/common.mk
@@ -147,6 +147,18 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/bin/getcap \
     system/bin/setcap
 
+# Enable blur
+TARGET_ENABLE_BLUR ?= true
+ifeq ($(TARGET_ENABLE_BLUR),true)
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.custom.blur.enable=true
+else
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.custom.blur.enable=false
+endif
+
+PRODUCT_SYSTEM_PROPERTIES += ro.surface_flinger.supports_background_blur=1
+
 # Filesystems tools
 PRODUCT_PACKAGES += \
     fsck.ntfs \
